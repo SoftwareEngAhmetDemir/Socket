6c90f50c7092c96a16b827ed6ce5f4ee036187eb
SoftwareEngAhmetDemir <deepdamer@gmail.com>
2020-11-12 22:26:10 +0300

    add

---
+++ index.js
@@ -0,0 +1,122 @@
+var app = require("express")();
+var cors = require("cors");
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var mresult = [];
+app.use(cors());
+
+app.get('/gettarih/:ilanid',(req,res)=>{
+  var MongoClient = require("mongodb").MongoClient;
+  var url =
+    "mongodb+srv://murat:murat3838@cluster0-2yepv.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("test");
+      var query = { ilanod: req.params.ilanid };
+      dbo.collection("estate").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      });
+    })
+
+
+})
+
+
+app.get('/update/:ilanid/:userid/:username/:fiyat',(req,res)=>{
+  var MongoClient = require("mongodb").MongoClient;
+  
+  var url ="mongodb+srv://murat:murat3838@cluster0-2yepv.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+   
+  MongoClient.connect(url, function(err, db) {
+    var dbo = db.db("test");
+  var myquery = { ilanid:req.params.ilanid };
+  var newvalues = {$set: {
+    ilanid: req.params.ilanid,
+    userid: req.params.userid,
+    username: req.params.username,
+    fiyat: req.params.fiyat,
+  } };
+  dbo.collection("teklif").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log(res.result.nModified + " document(s) updated");
+      io.emit('name',req.params.fiyat);
+      db.close();
+    });
+  })
+
+res.end('end');
+})
+
+
+
+
+app.get("/:ilanid", async (req, res) => {
+  var MongoClient = require("mongodb").MongoClient;
+  var url =
+    "mongodb+srv://murat:murat3838@cluster0-2yepv.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+
+  var t = [];
+  await new Promise((resolve, rej) => {
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("test");
+      console.log(req.params.ilanid);
+      var query = { ilanid: req.params.ilanid };
+      dbo
+        .collection("teklif")
+        .find(query)
+        .toArray(async function (err, result) {
+          if (err) throw err;
+          t = result;
+          // io.emit("name", t[0].fiyat);
+          resolve("");
+
+          db.close();
+        });
+    });
+  }).then(async (resolve) => {
+    if (t.length === 0) {
+      await io.emit("name", "dahayok");
+      res.end("");
+    } else {
+      await io.emit("name", t[0].fiyat);
+      res.end("");
+    }
+  });
+});
+
+// ///////////////////////
+app.get("/insertfiyat/:ilanid/:userid/:username/:fiyat", async (req, res) => {
+  var mresult = [];
+  await io.emit("name", req.params.fiyat);
+  var MongoClient = require("mongodb").MongoClient;
+  var url =
+    "mongodb+srv://murat:murat3838@cluster0-2yepv.gcp.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+
+  // if(mresult.length===0)
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+
+    var myobj = {
+      ilanid: req.params.ilanid,
+      userid: req.params.userid,
+      username: req.params.username,
+      fiyat: req.params.fiyat,
+    };
+    var dbo = db.db("test");
+    dbo.collection("teklif").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+
+      db.close();
+    });
+  });
+  res.end('');
+});
+
+// ///////////////////
+http.listen(8000, () => {
+  console.log("listening on *:6000");
+});
